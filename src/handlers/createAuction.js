679e269af5d8a482d6a73c9ead3d480075845bb9
shradheyt <shradhey029tripathi@gmail.com>
2020-07-08 08:16:49 +0530

    Initial Commit

---
+++ src/handlers/createAuction.js
@@ -0,0 +1,19 @@
+async function createAuction(event, context) {
+
+  const body = JSON.parse(event.body);
+  const now = new Date();
+
+  const auction = {
+    title : body.title,
+    status: 'OPEN',
+    createdAt : now.toISOString(),
+  };
+  return {
+    statusCode: 201,
+    body: JSON.stringify(auction),
+  };
+}
+
+export const handler = createAuction;
+
+
